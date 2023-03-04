54751da6be4db8791a075976ccceabaf9e9f5118
sophiamrobles <srobles2@hawk.iit.edu>
2023-03-03 20:21:20 -0600

    couldn't get this to function correctly

--- js/script.js
+++ js/script.js
@@ -2,13 +2,3 @@ jQuery(function($) {
     $('html').removeClass('nojs');
     $('html').addClass('hasjs');
 });
-function init(){
-    var input = document.getElementById('input-name')
-    var pressButton = document.getElementById('submit')
-
-    function alertFunc(){
-        alert('Thank you for joining, ' + input.value)
-    }
-pressButton.addEventListener('click', alertFunc);
-}
-window.addEventListener('load', init)
