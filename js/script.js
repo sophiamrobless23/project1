0487663c748ff89fb5e55d13532f60bafb5b4070
sophiamrobles <srobles2@hawk.iit.edu>
2023-03-31 00:28:52 -0500

    fix renaming issues, restructure alerts

--- js/script.js
+++ js/script.js
@@ -2,14 +2,13 @@ jQuery(function($) {
     $('html').removeClass('nojs');
     $('html').addClass('hasjs');
 });
-function validate(){
-    var name = document.getElementById('input-name')
-    var submission = document.getElementById('submit')
-    function alertFunc(){
-        alert('Successful submision for '+ name.value)
-        output.innerHTML='Thank you for joining' + name.value;
-    }
-    pressButton.addEventListener('click', alertFunc)
-}
-window.addEventListener('load', validate)
+function init(){
+    var name = index.getElementById('input-name')
+    var pressButton = index.getElementById('submit')
 
+    function alertFunc(){
+        alert('Thanks, ' + name.value)
+    }
+pressButton.addEventListener('click', alertFunc);
+}
+window.addEventListener('load', init)
