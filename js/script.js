ce04c8a6975463f70bcbc5aa68e6e5313d412ec6
sophiamrobles <srobles2@hawk.iit.edu>
2023-03-30 23:08:58 -0500

    add js validation ?

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@ jQuery(function($) {
     $('html').removeClass('nojs');
     $('html').addClass('hasjs');
 });
+function validate(){
+    var name = document.getElementById('input-name')
+    var submission = document.getElementById('submit')
+    function alertFunc(){
+        alert('Successful submision for '+ name.value)
+        output.innerHTML='Thank you for joining' + name.value;
+    }
+    pressButton.addEventListener('click', alertFunc)
+}
+window.addEventListener('load', validate)
+
